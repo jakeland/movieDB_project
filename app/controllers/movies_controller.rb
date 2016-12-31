570755dd2540f3bb0be0cb119dc8ba6244fb8476
jakeland <jakemc@embarqmail.com>
2016-12-31 13:24:48 -0800

    created images for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /movies
   # GET /movies.json
@@ -11,11 +12,11 @@ class MoviesController < ApplicationController
   # GET /movies/1.json
   def show
   end
-
-  # GET /movies/new
   def new
-    @movie = Movie.new
+    @movie = current_user.movies.build
   end
+  # GET /movies/new
+ 
 
   # GET /movies/1/edit
   def edit
@@ -24,7 +25,7 @@ class MoviesController < ApplicationController
   # POST /movies
   # POST /movies.json
   def create
-    @movie = Movie.new(movie_params)
+    @movie = current_user.movies.build(movie_params)
 
     respond_to do |format|
       if @movie.save
@@ -69,6 +70,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:title, :description, :movie_length, :director, :movie_rating, :avg_rating)
+      params.require(:movie).permit(:title, :description, :movie_length, :director, :movie_rating, :avg_rating, :image)
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #needed for actual host of application for devise
+  #JMC - add from dev, but for heroku
+  
 end
